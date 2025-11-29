92d6f74df1e0c46ded3d18b59f8e7e753e94477a
antonio cafasso <antonio.cafasso@mia-platform.eu>
2025-11-29 12:10:13 +0100

    Reverted header

--- app/_components/ArchivePlayer.js
+++ app/_components/ArchivePlayer.js
@@ -27,7 +27,7 @@ export default function ArchivePlayer({ archiveId, embedUrl, title }) {
         </div>
       )}
       <iframe
-        src={`${embedUrl}?showInfo=0`}
+        src={embedUrl}
         width="100%"
         height="100%"
         frameBorder="0"
